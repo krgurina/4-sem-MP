975c754f5be28d6712d81ac2934681538a1fc9a2
Kristina <kristina.gurina.03@gmail.com>
2023-08-06 22:22:39 +0300

    1

---
+++ 1 лаба/lab1/lab1/lab1.cpp
@@ -0,0 +1,69 @@
+﻿
+#include <iostream>
+#include "Auxil.h"                            
+#include <ctime>
+#include <locale>
+
+
+int  d = 0;
+int fibo(int n)
+{
+	d++;
+	if (n == 1 || n == 2) return (n - 1);
+	return fibo(n - 1) + fibo(n - 2);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+	
+	int cycle = 500000;
+	int av1 = 0;
+	double av2 = 0;
+	clock_t t1 = 0;
+	clock_t t2 = 0;
+	
+
+	auxil::start();     // старт генерации                     
+
+	for (int i = 0; i < 6; i++)
+	{
+		t1 = clock();    // фиксация времени                         
+		for (int i = 0; i < cycle; i++)
+		{
+			av1 += (double)auxil::iget(0, 100); // сумма случайных чисел 
+			av2 += auxil::dget(-100, 100);         // сумма случайных чисел 
+		}
+
+		t2 = clock();   // фиксация времени                         
+
+		
+		std::cout << std::endl << "количество циклов:         " << cycle;
+		std::cout << std::endl << "среднее значение (int):    " << av1 / cycle;
+		std::cout << std::endl << "среднее значение (double): " << av2 / cycle;
+		std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
+		std::cout << std::endl << "                  (сек):   "
+			<< ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC);
+		std::cout << std::endl;
+		cycle += 500000;
+	}
+
+	//3
+		t1 = clock();///фиксация t
+		for (int i = 0; i < 100; i++)
+		{
+			fibo(10);
+		}
+		t2 = clock();
+
+		std::cout << std::endl << "количество циклов:         " << 100;
+		std::cout << std::endl << "среднее значение (int):    " << av1 / 100;
+		std::cout << std::endl << "среднее значение (double): " << av2 / 100;
+		std::cout << std::endl << "продолжительность (у.е):   " << (t2 - t1);
+		std::cout << std::endl << "продолжительность (сек):   "
+			<< ((double)(t2 - t1)) / ((double)CLOCKS_PER_SEC) << "\n\n";
+		
+
+	return 0;
+}
+
---
+++ 2 лаба/MP_Lab2/Boat/MainBoat.cpp
@@ -0,0 +1,100 @@
+﻿//#include <iostream>
+//#include <iomanip>
+//#include "Boat.h"
+//#include "Auxil.h"
+//#include <ctime>
+//#include <locale>
+//#define MM 5
+//#define NN 25
+//
+//int wmain()
+//{
+//    setlocale(LC_ALL, "rus");
+//    int V = 1500,
+//        c[NN] = { 110, 15, 20, 125, 30,  25, 120, 144, 120, 25, 100,  25, 60, 55, 90, 125, 30, 35, 80, 47, 70, 45, 50, 95, 60};
+//    short  r[MM];
+//    int v[NN];
+//    auxil::start();     // старт генерации     
+//    for (int i = 0; i < NN; i++)
+//    {
+//        v[i]= auxil::iget(100, 900);
+//    }
+//    int cc = boat(
+//        V,   // [in]  максимальный вес груза
+//        MM,  // [in]  количество мест для контейнеров     
+//        NN,  // [in]  всего контейнеров  
+//        v,   // [in]  вес каждого контейнера  
+//        c,   // [in]  доход от перевозки каждого контейнера     
+//        r    // [out] результат: индексы выбранных контейнеров  
+//    );
+//    std::cout << std::endl << "- Задача о размещении контейнеров на судне";
+//    std::cout << std::endl << "- общее количество контейнеров    : " << NN;
+//    std::cout << std::endl << "- количество мест для контейнеров : " << MM;
+//    std::cout << std::endl << "- ограничение по суммарному весу  : " << V;
+//    std::cout << std::endl << "- вес контейнеров                 : ";
+//    for (int i = 0; i < NN; i++) std::cout << std::setw(3) << v[i] << " ";
+//    std::cout << std::endl << "- доход от перевозки              : ";
+//    for (int i = 0; i < NN; i++) std::cout << std::setw(3) << c[i] << " ";
+//    std::cout << std::endl << "- выбраны контейнеры (0,1,...,m-1): ";
+//    for (int i = 0; i < MM; i++) std::cout << r[i] << " ";
+//    std::cout << std::endl << "- доход от перевозки              : " << cc;
+//    std::cout << std::endl << "- общий вес выбранных контейнеров : ";
+//    int s = 0; for (int i = 0; i < MM; i++) s += v[r[i]]; std::cout << s;
+//    std::cout << std::endl << std::endl;
+//    system("pause");
+//    return 0;
+//}
+
+
+
+
+//
+//// ============================  ЗАВИСИМОСТЬ ВРЕМЕНИ ВЫЧИСЛЕНИЯ  ============================
+//
+
+#include <iostream>
+#include <iomanip>
+#include "Boat.h"
+#include <time.h>
+#include "Auxil.h"
+#define NN 35
+#define MM 6
+#define SPACE(n) std::setw(n)<<" "
+int wmain()
+{
+    setlocale(LC_ALL, "rus");
+    int V = 1500;
+    int v[NN];
+    int c[NN];
+    short r[MM];
+
+    auxil::start();
+    for (int i = 0; i < NN; i++)
+    {
+        v[i]= auxil::iget(100, 900);
+    }
+
+    for (int i = 0; i < NN; i++)
+    {
+        c[i] = auxil::iget(10, 150);
+    }
+
+    int maxcc = 0;
+    clock_t t1, t2;
+    std::cout << std::endl << "-- Задача об оптимальной загрузке судна -- ";
+    std::cout << std::endl << "-  ограничение по весу    : " << V;
+    std::cout << std::endl << "-  количество мест        : " << MM;
+    std::cout << std::endl << "-- количество ------ продолжительность -- ";
+    std::cout << std::endl << "   контейнеров        вычисления  ";
+    for (int i = 25; i <= NN; i++)
+    {
+        t1 = clock();
+        int maxcc = boat(V, MM, i, v, c, r);
+        t2 = clock();
+        std::cout << std::endl << SPACE(7) << std::setw(2) << i
+            << SPACE(15) << std::setw(5) << (t2 - t1);
+    }
+    std::cout << std::endl << std::endl;
+    system("pause");
+    return 0;
+}
